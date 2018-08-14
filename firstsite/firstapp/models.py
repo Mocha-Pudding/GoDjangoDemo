c82750baa2698e8e72e27601d178327af98b07d3
Mocha-Pudding <zhujiangge6@126.com>
2018-08-14 21:26:31 +0800

    GET  method
    用Get方法实现文章分类功能.

--- firstsite/firstapp/models.py
+++ firstsite/firstapp/models.py
@@ -10,5 +10,10 @@ class People(models.Model):
 class Article(models.Model):
     headline = models.CharField(null=True, blank=True, max_length=500)
     content = models.TextField(null=True, blank=True)
+    TAG_CHOICES = {
+    ('tech', 'Tech'),
+    ('life', 'Life'),
+    }
+    tag = models.CharField(null=True, blank=True, max_length=5, choices=TAG_CHOICES)
     def __str__(self):
         return self.headline
--- firstsite/firstapp/views.py
+++ firstsite/firstapp/views.py
@@ -14,7 +14,7 @@ def first_try(request):
             <body>
                 <h1 class="ui center aligned icon header">
                     <i class="hand spock icon"></i>
-                    Hello, {{ person.name}}
+                    Hello, {{ person.name }}
                 </h1>
             </body>
         </html>
@@ -26,8 +26,19 @@ def first_try(request):
     return HttpResponse(web_page)
 
 def index(request):
+    print(request)
+    print('==='*30)
+    print(dir(request))
+    print('==='*30)
+    print(type(request))
+    queryset = request.GET.get('tag')
+    if queryset:
+        article_list = Article.objects.filter(tag=queryset)
+    else:
+        article_list = Article.objects.all()
+    print(queryset)
     context = {};
-    article_list = Article.objects.all()
+
     context['article_list'] = article_list
     index_page = render(request, 'first_web_2.html', context)
     return index_page
